b549cff95dca241c14e1bc6e313a4cb3f1312f80
bhargav0425 <patelbhargav2510@gmail.com>
2023-08-30 12:23:17 -0400

    Trackrecord section updated -  commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,239 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+// import { AppBar, Tabs, Tab, Container, Typography, Box, useScrollTrigger } from '@mui/material';
+// import Table from '@mui/material/Table';
+// import TableBody from '@mui/material/TableBody';
+// import TableCell from '@mui/material/TableCell';
+// import TableContainer from '@mui/material/TableContainer';
+// import TableHead from '@mui/material/TableHead';
+// import TableRow from '@mui/material/TableRow';
+// import Paper from '@mui/material/Paper';
+// import ImageTextComponent from './ImageTextComponent';
+
+// function App() {
+//   const [value, setValue] = useState(0);
+
+//   const handleChange = (event, newValue) => {
+//     setValue(newValue);
+//     const elementId = getTabElementId(newValue);
+//     const element = document.getElementById(elementId);
+//     element?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+//   };
+
+//   const getTabElementId = (index) => {
+//     const tabs = ['overview', 'track-record', 'investment-strategy', 'esg', 'impact-invest', 'team'];
+//     return tabs[index];
+//   };
+
+//   const trigger = useScrollTrigger({
+//     disableHysteresis: true,
+//     threshold: 100,
+//   });
+
+//   const columns = [
+//     {
+//       width: 200,
+//       label: 'Dessert',
+//       dataKey: 'dessert',
+//     },
+//     {
+//       width: 120,
+//       label: 'Calories\u00A0(g)',
+//       dataKey: 'calories',
+//       numeric: true,
+//     },
+//     {
+//       width: 120,
+//       label: 'Fat\u00A0(g)',
+//       dataKey: 'fat',
+//       numeric: true,
+//     },
+//     {
+//       width: 120,
+//       label: 'Carbs\u00A0(g)',
+//       dataKey: 'carbs',
+//       numeric: true,
+//     },
+//     {
+//       width: 120,
+//       label: 'Protein\u00A0(g)',
+//       dataKey: 'protein',
+//       numeric: true,
+//     },
+//   ];
+
+//   const sample = [
+//     ['Frozen yoghurt', 159, 6.0, 24, 4.0],
+//     ['Ice cream sandwich', 237, 9.0, 37, 4.3],
+//     ['Eclair', 262, 16.0, 24, 6.0],
+//     ['Cupcake', 305, 3.7, 67, 4.3],
+//     ['Gingerbread', 356, 16.0, 49, 3.9],
+//   ];
+//   // const rows = Array.from({ length: 200 }, (_, index) => {
+//   //   const randomSelection = sample[Math.floor(Math.random() * sample.length)];
+//   //   return createData(index, ...randomSelection);
+//   // });
+
+//   return (
+//     <div>
+//       {/* Banner */}
+//       <Container maxWidth="md">
+//         <Typography variant="h2" gutterBottom>
+//           Your Banner Here
+//         </Typography>
+//       </Container>
+
+//       {/* Tabs */}
+//       <AppBar position={trigger ? 'fixed' : 'static'} color="default">
+//         <Tabs value={value} onChange={handleChange}>
+//           <Tab label="Overview" />
+//           <Tab label="Track Record" />
+//           <Tab label="Investment Strategy" />
+//           <Tab label="ESG" />
+//           <Tab label="Impact Invest" />
+//           <Tab label="Team" />
+//         </Tabs>
+//       </AppBar>
+
+//       <Container>
+//         <Box id="overview" my={4}>
+//           <Typography variant="h4">Overview</Typography>
+//           <Typography>Overview content here.</Typography>
+//           <Typography>
+//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
+//             pulvinar facilisis justo mollis, auctor consequat urna.
+//           </Typography>
+//           <Typography>
+//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
+//             pulvinar facilisis justo mollis, auctor consequat urna.
+//           </Typography>
+//           <Typography>
+//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
+//             pulvinar facilisis justo mollis, auctor consequat urna.
+//           </Typography>
+//         </Box>
+
+//         <Box id="track-record" my={4}>
+//           <Typography variant="h4">Track Record</Typography>
+//           <Typography>Track record content here.</Typography>
+//           <TableRow>
+            
+//           {columns.map((column) => (
+//           <TableCell
+//           key={column.dataKey}
+//           variant="head"
+//           align={column.numeric || false ? 'right' : 'left'}
+//           style={{ width: column.width }}
+//           sx={{
+//             backgroundColor: 'background.paper',
+//           }}
+//           >
+//           {column.label}
+//           </TableCell>
+//           ))}
+//           </TableRow>
+
+//           <Container maxWidth="md">
+//           {/* Image Componenet one */}
+//         <ImageTextComponent
+//         videoSrc="path/to/your/video.mp4"
+//         title="Video Title"
+//         content="Content for Video"
+//         />
+
+//         <ImageTextComponent
+//         videoSrc="path/to/your/video.mp4"
+//         title="Video Title"
+//         content="Content for Video"
+//         />
+//     </Container>
+//         </Box>
+
+      
+//         <Box id="investment-strategy" my={4}>
+//           <Typography variant="h4">Investment strategy</Typography>
+//           <Typography>Overview content here.</Typography>
+//           <Typography>
+//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
+//             pulvinar facilisis justo mollis, auctor consequat urna.
+//           </Typography>
+//           <Typography>
+//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
+//             pulvinar facilisis justo mollis, auctor consequat urna.
+//           </Typography>
+//           <Typography>
+//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
+//             pulvinar facilisis justo mollis, auctor consequat urna.
+//           </Typography>
+//         </Box>
+
+//         <Box id="esg" my={4}>
+//           <Typography variant="h4">esg and impact</Typography>
+//           <Typography>Overview content here.</Typography>
+//           <Typography>
+//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
+//             pulvinar facilisis justo mollis, auctor consequat urna.
+//           </Typography>
+//           <Typography>
+//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
+//             pulvinar facilisis justo mollis, auctor consequat urna.
+//           </Typography>
+//           <Typography>
+//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
+//             pulvinar facilisis justo mollis, auctor consequat urna.
+//           </Typography>
+//         </Box>
+
+//         <Box id="impact-invest" my={4}>
+//           <Typography variant="h4">impact invest</Typography>
+//           <Typography>Track record content here.</Typography>
+//           <Typography>
+//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
+//             pulvinar facilisis justo mollis, auctor consequat urna.
+//           </Typography>
+//           <Typography>
+//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
+//             pulvinar facilisis justo mollis, auctor consequat urna.
+//           </Typography>
+//           <Typography>
+//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
+//             pulvinar facilisis justo mollis, auctor consequat urna.
+//           </Typography>
+//         </Box>
+
+//         <Box id="team" my={4}>
+//           <Typography variant="h4">Team</Typography>
+//           <Typography>Track record content here.</Typography>
+//           <Typography>
+//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
+//             pulvinar facilisis justo mollis, auctor consequat urna.
+//           </Typography>
+//           <Typography>
+//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
+//             pulvinar facilisis justo mollis, auctor consequat urna.
+//           </Typography>
+//           <Typography>
+//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
+//             pulvinar facilisis justo mollis, auctor consequat urna.
+//           </Typography>
+//         </Box>
+
+//         {/* Add other sections similarly */}
+//       </Container>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState, useEffect, useRef } from 'react';
 import { AppBar, Tabs, Tab, Container, Typography, Box, useScrollTrigger } from '@mui/material';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import ImageTextComponent from './ImageTextComponent';
+import OverviewSection from './Component/OverviewSection';
+import TrackRecordSection from './Component/TrackRecordSection';
+
 
 function App() {
   const [value, setValue] = useState(0);
+  const sectionsRef = useRef({});
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -29,49 +252,37 @@ function App() {
     threshold: 100,
   });
 
-  const columns = [
-    {
-      width: 200,
-      label: 'Dessert',
-      dataKey: 'dessert',
-    },
-    {
-      width: 120,
-      label: 'Calories\u00A0(g)',
-      dataKey: 'calories',
-      numeric: true,
-    },
-    {
-      width: 120,
-      label: 'Fat\u00A0(g)',
-      dataKey: 'fat',
-      numeric: true,
-    },
-    {
-      width: 120,
-      label: 'Carbs\u00A0(g)',
-      dataKey: 'carbs',
-      numeric: true,
-    },
-    {
-      width: 120,
-      label: 'Protein\u00A0(g)',
-      dataKey: 'protein',
-      numeric: true,
-    },
-  ];
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const sectionId = entry.target.id;
+            const index = getTabIndex(sectionId);
+            setValue(index);
+          }
+        });
+      },
+      {
+        rootMargin: '-20% 0px -79% 0px',
+      }
+    );
 
-  const sample = [
-    ['Frozen yoghurt', 159, 6.0, 24, 4.0],
-    ['Ice cream sandwich', 237, 9.0, 37, 4.3],
-    ['Eclair', 262, 16.0, 24, 6.0],
-    ['Cupcake', 305, 3.7, 67, 4.3],
-    ['Gingerbread', 356, 16.0, 49, 3.9],
-  ];
-  // const rows = Array.from({ length: 200 }, (_, index) => {
-  //   const randomSelection = sample[Math.floor(Math.random() * sample.length)];
-  //   return createData(index, ...randomSelection);
-  // });
+    Object.values(sectionsRef.current).forEach((section) => {
+      observer.observe(section);
+    });
+
+    return () => {
+      Object.values(sectionsRef.current).forEach((section) => {
+        observer.unobserve(section);
+      });
+    };
+  }, []);
+
+  const getTabIndex = (sectionId) => {
+    const tabs = ['overview', 'track-record', 'investment-strategy', 'esg', 'impact-invest', 'team'];
+    return tabs.indexOf(sectionId);
+  };
 
   return (
     <div>
@@ -95,61 +306,34 @@ function App() {
       </AppBar>
 
       <Container>
-        <Box id="overview" my={4}>
-          <Typography variant="h4">Overview</Typography>
-          <Typography>Overview content here.</Typography>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
-            pulvinar facilisis justo mollis, auctor consequat urna.
-          </Typography>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
-            pulvinar facilisis justo mollis, auctor consequat urna.
-          </Typography>
+        <Box ref={(el) => (sectionsRef.current['overview'] = el)} id="overview" my={4}>
+          <OverviewSection/>
+          
           <Typography>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
             pulvinar facilisis justo mollis, auctor consequat urna.
           </Typography>
         </Box>
 
-        <Box id="track-record" my={4}>
+        <Box ref={(el) => (sectionsRef.current['track-record'] = el)} id="track-record" my={4}>
           <Typography variant="h4">Track Record</Typography>
+          <TrackRecordSection/>
           <Typography>Track record content here.</Typography>
-          <TableRow>
-            
-          {columns.map((column) => (
-          <TableCell
-          key={column.dataKey}
-          variant="head"
-          align={column.numeric || false ? 'right' : 'left'}
-          style={{ width: column.width }}
-          sx={{
-            backgroundColor: 'background.paper',
-          }}
-          >
-          {column.label}
-          </TableCell>
-          ))}
-          </TableRow>
-
-          <Container maxWidth="md">
-          {/* Image Componenet one */}
-        <ImageTextComponent
-        videoSrc="path/to/your/video.mp4"
-        title="Video Title"
-        content="Content for Video"
-        />
-
-        <ImageTextComponent
-        videoSrc="path/to/your/video.mp4"
-        title="Video Title"
-        content="Content for Video"
-        />
-    </Container>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
+            pulvinar facilisis justo mollis, auctor consequat urna.
+          </Typography>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
+            pulvinar facilisis justo mollis, auctor consequat urna.
+          </Typography>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
+            pulvinar facilisis justo mollis, auctor consequat urna.
+          </Typography>
         </Box>
 
-      
-        <Box id="investment-strategy" my={4}>
+        <Box ref={(el) => (sectionsRef.current['investment-strategy'] = el)} id="investment-strategy" my={4}>
           <Typography variant="h4">Investment strategy</Typography>
           <Typography>Overview content here.</Typography>
           <Typography>
@@ -166,7 +350,7 @@ function App() {
           </Typography>
         </Box>
 
-        <Box id="esg" my={4}>
+        <Box ref={(el) => (sectionsRef.current['esg'] = el)} id="esg" my={4}>
           <Typography variant="h4">esg and impact</Typography>
           <Typography>Overview content here.</Typography>
           <Typography>
@@ -183,7 +367,7 @@ function App() {
           </Typography>
         </Box>
 
-        <Box id="impact-invest" my={4}>
+        <Box ref={(el) => (sectionsRef.current['impact-invest'] = el)} id="impact-invest" my={4}>
           <Typography variant="h4">impact invest</Typography>
           <Typography>Track record content here.</Typography>
           <Typography>
@@ -200,7 +384,7 @@ function App() {
           </Typography>
         </Box>
 
-        <Box id="team" my={4}>
+        <Box ref={(el) => (sectionsRef.current['team'] = el)} id="team" my={4}>
           <Typography variant="h4">Team</Typography>
           <Typography>Track record content here.</Typography>
           <Typography>
